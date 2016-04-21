f169b7fe6efa5d22fab09414c110b158e8ceaa04
Ngô Sỹ Trường <truongfatty94@gmai.com>
2016-04-21 15:54:33 +0700

    Initial Commit

---
+++ NestedSquare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NestedSquare
+//
+//  Created by Ngô Sỹ Trường on 4/21/16.
+//  Copyright © 2016 ngotruong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NestedSquare/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NestedSquare
+//
+//  Created by Ngô Sỹ Trường on 4/21/16.
+//  Copyright © 2016 ngotruong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
